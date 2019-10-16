47e769dd73bed52ca20f6158ba979229cf792bad
olgaloboda <olga.loboda.13@gmail.com>
2019-10-16 11:00:49 +0100

    added a hardcoded accessibility statement link

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,8 +1,13 @@
 <template>
   <footer class="footer">
-		<img src="../assets/images/kings-college.png" alt="Kings college London logo"  class="footer__img">
-		<img src="../assets/images/royal-trust.png" alt="Royal collection trust logo" class="footer__img">
-		<img src="../assets/images/omohundro-insitute.png" alt="Omohundro institute logo" class="footer__img">
-		<img src="../assets/images/will-mary.png" alt="William and Mary logo" class="footer__img">
+		<div>
+			<img src="../assets/images/kings-college.png" alt="Kings college London logo"  class="footer__img">
+			<img src="../assets/images/royal-trust.png" alt="Royal collection trust logo" class="footer__img">
+			<img src="../assets/images/omohundro-insitute.png" alt="Omohundro institute logo" class="footer__img">
+			<img src="../assets/images/will-mary.png" alt="William and Mary logo" class="footer__img">
+		</div>
+		<p><a href="/accessibility-statement">Accessibility statement</a>
+		<span class="delimiter">|</span>
+		© 2019 Copyright THE GEORGIAN PAPERS PROGRAMME</p>
 	</footer>
 </template>
