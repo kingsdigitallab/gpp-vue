818c3d90e7a926a26d969c8ffe0781cbe6e18856
Jamie Norrish <jamie@artefact.org.nz>
2021-01-05 15:39:58 +1300

    Removed unnecessary project-specifying element to API URLs. Moved API_URL to be read from the environment.

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,36 +1,35 @@
 import axios from 'axios';
 
 export const TOKEN = process.env.VUE_APP_API_TOKEN;
-const API_URL = "https://gpp-autharch-stg.kdl.kcl.ac.uk/api";
-const PROJECT_PARAM = "?project=gpp";
+const API_URL = process.env.VUE_APP_API_URL;
 
 async function getUrl(url) {
-    return axios.get(url, { 
+    return axios.get(url, {
         headers: getHeaders()
     });
 }
 
 async function get(action) {
-    return axios.get(`${API_URL}${action}${PROJECT_PARAM}`, {
+    return axios.get(`${API_URL}${action}`, {
         headers: getHeaders()
     });
 }
 
 async function getSingle(action, id) {
-    return axios.get(`${API_URL}${action}/${id}${PROJECT_PARAM}`, {
+    return axios.get(`${API_URL}${action}/${id}/`, {
         headers: getHeaders()
     });
 }
 
 async function getFilteredData(action, filters) {
-    return axios.get(`${API_URL}${action}${PROJECT_PARAM}&${filters}`, {
+    return axios.get(`${API_URL}${action}?${filters}`, {
         headers: getHeaders()
     });
 }
 
 function getHeaders() {
     return {
-        Bearer: `Token ${TOKEN}`,
+        Authorization: `Token ${TOKEN}`,
     }
 }
 
